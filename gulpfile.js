6c32e3c86222b13c9b925da430c2663bf5615e40
= <=>
2016-09-22 17:44:24 +0200

    package.json - dodane: gulp, angular, angular-ui

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+/**
+ * Created by marcinp on 07.09.16.
+ */
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create(); //auto-odswiezanie przegladarki
+
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server : {
+            baseDir : './'
+        }
+    })
+});
+
+gulp.task('watch', ['browserSync'], function () {
+    gulp.watch('./*.*', browserSync.reload);
+});
